8787461f58f323c235cfba83e61861c88b69d314
YvonneThoy <thoyyvonne19@gmail.com>
2021-05-25 23:32:20 +0800

    Update 7.-Lid-Driven-Cavity.jl

--- test/literate/7.-Lid-Driven-Cavity.jl
+++ test/literate/7.-Lid-Driven-Cavity.jl
@@ -7,29 +7,32 @@ using ViscousFlow
 using Plots
 
 #=
-##Problem specification
+## Problem specification
 Take $Re=100$ for example. We will set the Reynolds number to 100
 =#
 Re = 100 # Reynolds number
 
 #=
-##Discretization
+## Discretization
 Note that the rectangle function used for making the cavity shape requires a specified half length. The immersed boundary projection method for internal flow requires the size of the domain to be at least a step size greater at the boundaries (i.e. half length + Δx).
 =#
 Δt,Δx = setstepsizes(Re,gridRe=1.6)
-halflength=0.5 # to make rectangle with side length of 1
+#-
+halflength=0.5 # make rectangle with side length of 1
 domain_lim=halflength+1.01*Δx; # 1.01 is just an abitrary factor chosen to be greater than 1
-xlim, ylim = (-domain_lim,domain_lim),(-domain_lim,domain_lim)
+#-
+xlim = (-domain_lim,domain_lim)
+ylim = (-domain_lim,domain_lim)
 
 #= 
-##Cavity Geometry
+## Cavity Geometry
 A square cavity can be created using the $rectangle()$ function with the half length defined above.
 =#
-body = Rectangle(halflength,halflength,1.5*Δx) 
+body = Rectangle(halflength,halflength,1.5*Δx,shifted=true) 
 plot(body)
 
 #=
-##Boundary Condition at the moving wall
+## Boundary Condition at the moving wall
 Assign velocity to the top boundary.
 
 The $LidDrivenCavity()$ function can be used to specify the velocity value at the top wall.
@@ -39,7 +42,7 @@ Note : Non-dimensional velocity = 1
 m = ViscousFlow.LidDrivenCavity(1.0); # motion type
 
 #=
-##Construct the system structure
+## Construct the system structure
 The last two input "flow_side" and "static_points" must specified so the default setting in the $NavierStokes()$ function can be overwritten.
 
 "static_points" is true because the cavity is not moving.
@@ -54,13 +57,13 @@ u0 = newstate(sys)
 #=
 Set up integrator
 =#
-tspan = (0.0,100.0)
+tspan = (0.0,10.0)
 integrator = init(u0,tspan,sys)
 
 #=
 ## Solve
 =#
-step!(integrator,100)
+step!(integrator,10)
 
 #=
 ## Examine
@@ -69,9 +72,11 @@ plot for vorticity and streamlines
 =#
 
 plot(
-plot(vorticity(integrator),sys,title="Vorticity (Computed)",clim=(-5,5),levels=[-3,-2,-1,-0.5,0,0.344828,1,2,3,5,6],color=:turbo,linewidth=1.5,ylim=ylim,fillrange=nothing),
-plot(streamfunction(integrator),sys,title="Streamlines (Computed)",levels=[-0.459174,-0.468933,-0.449415,-0.420139,-0.41,-0.429898,-0.439657,-0.4]),
+plot(vorticity(integrator),sys,title="Vorticity (Computed)",clim=(-5,5),levels=10,color=:turbo,linewidth=1.5,ylim=ylim,fillrange=nothing),
+plot(streamfunction(integrator),sys,title="Streamlines (Computed)",fillrange=nothing, color=:black),
    size=(700,300))
+
+#-
 
 sol = integrator.sol;
 @gif for (u,t) in zip(sol.u,sol.t)
